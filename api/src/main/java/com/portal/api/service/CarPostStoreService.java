b0567d546408b0282088a79a8062839ae5e258dd
Wesley Oliveira Silva <wesolsv@gmail.com>
2025-01-13 17:04:26 -0300

    iniciando projeto

---
+++ api/src/main/java/com/portal/api/service/CarPostStoreService.java
@@ -0,0 +1,16 @@
+package com.portal.api.service;
+
+import com.portal.api.dto.CarPostDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface CarPostStoreService {
+
+    List<CarPostDTO> getCarForSales();
+
+    void changeCarForSale(CarPostDTO carPostDTO, String id);
+
+    void removeCarForSale(String id);
+}
